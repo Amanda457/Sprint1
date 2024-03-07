5f2afc1e332a88e2d297996954080f7c12bf8b4e
Amanda457 <mandy96lomi@gmail.com>
2024-03-07 13:02:13 +0100

    Añado ex2

---
+++ Tema2/Nivell_1/Ex_2.php
@@ -0,0 +1,16 @@
+<?php
+$hola = "Hello, World!";
+echo $hola."<br>";
+//Passo a majúscules i imprimeixo:
+echo strtoupper($hola)."<br>";
+
+echo "La seva longitud és: ".strlen ("$hola")."<br>";
+echo strrev($hola)."<br>";
+
+$curs = " Aquest és el curs de PHP.";
+echo $hola.$curs;
+
+
+
+
+?>
