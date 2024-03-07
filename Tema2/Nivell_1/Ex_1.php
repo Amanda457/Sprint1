73187001e5ab2de55f1a2a457eb60f06e376a35b
Amanda457 <mandy96lomi@gmail.com>
2024-03-07 12:31:14 +0100

    añado ex1 de tema 1

---
+++ Tema2/Nivell_1/Ex_1.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = 5;
+$y = 2.55;
+$reskiling = "PHP";
+$student = true;
+
+echo $x . "<br>";
+echo $y . "<br>";
+echo $reskiling . "<br>";
+echo $student;
+
+define ("name", "Amanda");
+echo "<h1>".name."</h1>";
+?>
+
