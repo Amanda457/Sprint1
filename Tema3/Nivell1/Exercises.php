af1a7abf4d3757b53d9e987bcbc752b7c913bf0b
Amanda457 <mandy96lomi@gmail.com>
2024-03-12 13:29:06 +0100

    Exercises con ex2

--- Tema3/Nivell1/Exercises.php
+++ Tema3/Nivell1/Exercises.php
@@ -2,11 +2,17 @@
 echo "<h2> Exercici 1 </h2>";
 $numbers = array ( 3, 4, 5, 1, 7);
 
-foreach ($numbers as $x){
-    echo $x."<br>";
+foreach ($numbers as $y){
+    echo $y. "<br>";
 }
-echo "<h2> Exercici 2 </h2>";
+echo "<h2> Exercici 2 </h2>". PHP_EOL;
 
-
-
+$x = array (10, 20, 30, 40, 50,60);
+echo "La mida del array és de: ".count($x)."<br>";
+unset($x[2]);
+$x = array_values($x);
+/* Per comprobar que s'ha reindexat correctament:
+echo var_dump($x);
+*/
+echo "La mida del array és de: ".count($x)."<br>";
 ?>
