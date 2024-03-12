1b3d76d5af459d87a22968a7eb5e7cb5b266b04f
Amanda457 <mandy96lomi@gmail.com>
2024-03-12 10:14:58 +0100

    Ex3 a) repassat

---
+++ Tema2/Nivell_1/Ex_3.php
@@ -0,0 +1,31 @@
+<?php
+
+$x = 5;
+$y = 2;
+$n = 1.1;
+$m = 4.2;
+
+echo "La variable x té el valor de: " . $x."<br>";
+echo "La variable y té el valor de: " . $y."<br>";
+echo "La suma dona: ".$x + $y ."<br>";
+echo "La resta dona: ".$x - $y."<br>";
+echo "El producte dona: ".$x * $y."<br>";
+echo "El mòdul dona: ".$x % $y."<br><br>";
+
+echo "La variable n té el valor de: " . $n."<br>";
+echo "La variable m té el valor de: " . $m."<br>";
+echo "La suma dona: ". $n + $m ."<br>";
+echo "La resta dona: ".$n - $m."<br>";
+echo "El producte dona: ".$n * $m."<br>";
+echo "El mòdul dona: ".$n % $m."<br><br>";
+
+$doble = 2;
+echo "El doble de la variant x és: ".$x * $doble."<br>";
+echo "El doble de la variant y és: ".$y * $doble."<br>";
+echo "El doble de la variant n és: ".$n * $doble."<br>";
+echo "El doble de la variant m és: ".$m * $doble."<br>";
+
+echo "La suma de totes les variables dona el resultat de: " . ($x+$y+$n+$m) . "<br>";
+echo "El producte de totes les variables dona el resultat de: " . $x*$y*$n*$m .  "<br>";
+
+?>
