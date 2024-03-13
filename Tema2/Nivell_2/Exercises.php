c98e3986ea5cfe01320afa63d24c2bab06a91c2e
Amanda457 <mandy96lomi@gmail.com>
2024-03-13 19:12:04 +0100

    Pujo ex2 de nivell 2

--- Tema2/Nivell_2/Exercises.php
+++ Tema2/Nivell_2/Exercises.php
@@ -20,4 +20,38 @@ calcularCost(4.6);
 calcularCost(2);
 calcularCost(3.1);
 
+echo "<h2> Exercici 2 </h2>";
+
+
+function calcularCompra(int $xocolata, int $xiclet, int $caramel) : void{
+    $totalXoco = calcularXocolata($xocolata);
+    $totalXiclet = calcularXiclet($xiclet);
+    $totalCaramel = calcularCaramel($caramel);
+   
+    echo "El resum de la compra és de $totalXoco + $totalXiclet + $totalCaramel amb el total de: " . $totalXoco+$totalXiclet+$totalCaramel."<br>";
+}
+function calcularXocolata (int $xocolata) : int {
+    $preuXocolata = 1;
+    $costXoco = $xocolata * $preuXocolata;
+
+
+    return $costXoco;
+}
+
+function calcularXiclet (int $xiclet) : float {
+    $preuXicle = 0.5;
+    $costXicle = $xiclet * $preuXicle;
+
+    return $costXicle;
+}
+
+function calcularCaramel (int $caramel) : float {
+    $preuCaramel = 1.5;
+    $costCaramel = $caramel * $preuCaramel;
+
+    return $costCaramel;
+}
+
+calcularCompra(2, 4, 5);
+calcularCompra(2, 1, 1); //Com a l'enunciat de l'exercici.
 ?>
