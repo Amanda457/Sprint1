2bff19ba146801150cb323bacf4d32fc7f3e39fb
Amanda457 <mandy96lomi@gmail.com>
2024-03-13 10:49:42 +0100

    Modifico ex4 amb correccions

--- Tema2/Nivell_1/Ex_4.php
+++ Tema2/Nivell_1/Ex_4.php
@@ -1,20 +1,15 @@
 <?php
-function contar($numMax, $deCuanto){
-  /* No he posat el valor predefinit en el cas de no introduir per paràmetre el $numMax 
-  perquè són 2 arguments els que entren i no se com fer-ho, així que opto per un if.*/
-   
+function contar(int $deCuanto, int $numMax = 10){
+  
   $i = 0;
     while($i <= $numMax){
-         if ($numMax == null){
-         $numMax = 10;
-         }
-     echo $i."<br>";
+          echo $i."<br>";
      $i+= $deCuanto;
 
 }
 echo "<br>";
 }
 //Dos exemples de funcionament:
-contar(null, 2);
-contar(100, 10);
+contar(2);
+contar(10, 100);
 ?>
