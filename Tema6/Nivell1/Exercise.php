0d1837a79982f664d02eb1c0329a3a440e37821d
Amanda457 <mandy96lomi@gmail.com>
2024-03-19 11:22:47 +0100

    subo ex1

---
+++ Tema6/Nivell1/Exercise.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h3>Your answers:</h3>   
+Welcome <?php echo $_POST["name"]; ?><br>
+You live in: <?php echo $_POST["place"]; ?><br>
+You have  <?php echo $_POST["age"]; ?> years old.<br>
+Your zodiac sign is: <?php echo $_POST["zodiac"]; ?><br>
+Your favorite color is: <?php echo $_POST["color"];?>
+
+</body>
+</html>
