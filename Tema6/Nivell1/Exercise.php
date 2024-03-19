c07fd170b85b0f3f6e6c3ccd88f6dfe4e4134db2
Amanda457 <mandy96lomi@gmail.com>
2024-03-19 11:40:19 +0100

    Utilitzar la session i la seva variable superglobal (ex2)

--- Tema6/Nivell1/Exercise.php
+++ Tema6/Nivell1/Exercise.php
@@ -1,3 +1,7 @@
+<?php 
+session_start()
+?>
+
 <html>
 <body>
 <h3>Your answers:</h3>   
@@ -5,7 +9,15 @@ Welcome <?php echo $_POST["name"]; ?><br>
 You live in: <?php echo $_POST["place"]; ?><br>
 You have  <?php echo $_POST["age"]; ?> years old.<br>
 Your zodiac sign is: <?php echo $_POST["zodiac"]; ?><br>
-Your favorite color is: <?php echo $_POST["color"];?>
+Your favorite color is: <?php echo $_POST["color"];?><br>
+<br>
 
+<?php 
+//Configurem per a que el nom es guardi com a variable global dins de session:
+$_SESSION["name"] = $_POST["name"];
+
+//Accedim a la variable global:
+echo "Your name is " . $_SESSION["name"] . ".<br>";
+?>
 </body>
 </html>
